452453bf4d82b0eead06241abc57e6569b2f2994
Shubham <shubdev07@gmail.com>
2020-02-13 13:09:24 +0530

    testing commit

--- resources/user.py
+++ resources/user.py
@@ -1,20 +1,23 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 
+
 from models.user import UserModel
+
 
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
     parser.add_argument('password',
-        type=str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
     def post(self):
         data = UserRegister.parser.parse_args()
 
